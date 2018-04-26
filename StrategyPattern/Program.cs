edf60f97bac161ddc51f0bc419229a179517768c
Bruno Barrette <bbarrette@pyxis-tech.com>
2018-04-26 06:55:22 -0400

    Program with details

--- StrategyPattern/Program.cs
+++ StrategyPattern/Program.cs
@@ -7,23 +7,33 @@ namespace StrategyPattern
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
-
             var invoice = new Invoice();
             invoice.AddProduct(new Product(10, true));
             invoice.AddProduct(new Product(15, false));
             
             var total = invoice.GetInvoiceTotal(PurchaseMethod.Online, Province.Quebec);
-            total = invoice.GetInvoiceTotalWithStrategy(new OnlineInvoiceTotalStrategy());
-            Console.WriteLine($"Total si acheté en ligne: {total}");
-            
+            Console.WriteLine($"Total si acheté en ligne (sans Strategy): {total}");
+
             total = invoice.GetInvoiceTotal(PurchaseMethod.InStore, Province.Quebec);
-            total = invoice.GetInvoiceTotalWithStrategy(new QuebecStoreInvoiceTotalStrategy());
-            Console.WriteLine($"Total si acheté dans un magasin au Québec: {total}");
+            Console.WriteLine($"Total si acheté dans un magasin au Québec (sans Strategy): {total}");
 
             total = invoice.GetInvoiceTotal(PurchaseMethod.InStore, Province.Ontario);
+            Console.WriteLine($"Total si acheté dans un magasin au Québec (sans Strategy): {total}");
+
+            #region With Strategy Pattern
+
+            total = invoice.GetInvoiceTotalWithStrategy(new OnlineInvoiceTotalStrategy());
+            Console.WriteLine($"Total si acheté en ligne (avec Strategy): {total}");
+
+            total = invoice.GetInvoiceTotalWithStrategy(new QuebecStoreInvoiceTotalStrategy());
+            Console.WriteLine($"Total si acheté dans un magasin au Québec (avec Strategy): {total}");
+
             total = invoice.GetInvoiceTotalWithStrategy(new OntarioStoreInvoiceTotalStrategy());
-            Console.WriteLine($"Total si acheté dans un magasin en Ontario: {total}");
+            Console.WriteLine($"Total si acheté dans un magasin en Ontario (avec Strategy): {total}");
+
+            #endregion
+            
+            Console.ReadKey();
         }
     }
 }
